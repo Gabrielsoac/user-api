222d11e230e2ea5b8ed5d1de456ec9d8a5ad0e56
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-21 17:34:07 -0300

    Feat: Create Class for to receive CEP data

---
+++ src/main/java/com/user_api/clients/CEPClient.java
@@ -0,0 +1,13 @@
+package com.user_api.clients;
+
+import com.user_api.DTOs.ResponseAddress;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "CEPClient", url = "https://api.postmon.com.br/v1/cep/")
+public interface CEPClient {
+
+    @GetMapping("/{CEP}")
+    ResponseAddress getAddress(@PathVariable String CEP);
+}
