cfc83ab485c82196beb913188244e4f6be78449c
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-21 17:31:09 -0300

    Feat: Added username and password attributes

--- src/main/java/com/user_api/model/entities/User.java
+++ src/main/java/com/user_api/model/entities/User.java
@@ -4,20 +4,40 @@ package com.user_api.model.entities;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
-@Entity(name="users")
+@Entity
 @Table(name="users")
 @NoArgsConstructor
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    private String username;
+    private String password;
     private String name;
     private String email;
 
     @ManyToOne
     private Address address;
 
-    public User(String name, String email, Address address) {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String username, String password, String name, String email, Address address) {
+        this.username = username;
+        this.password = password;
         this.name = name;
         this.email = email;
         this.address = address;
