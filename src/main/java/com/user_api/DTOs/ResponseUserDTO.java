019d7155ad07d76e39e027a53221ab4664237508
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-22 11:36:17 -0300

    Fix: added field username in ResponseUserDTO

--- src/main/java/com/user_api/DTOs/ResponseUserDTO.java
+++ src/main/java/com/user_api/DTOs/ResponseUserDTO.java
@@ -2,5 +2,5 @@ package com.user_api.DTOs;
 
 import com.user_api.model.entities.Address;
 
-public record ResponseUserDTO (String name, String email, Address address){
+public record ResponseUserDTO (String username, String name, String email, Address address){
 }
