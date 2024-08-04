0867799c4a4e619ccf63e96a99196107fda6845f
Gabriel Soares <gabrielsoacc@gmail.com>
2024-08-04 11:53:41 -0300

    feat: alter table users

--- src/main/resources/db/migration/V1__CREATE_TABLE_USERS.sql
+++ src/main/resources/db/migration/V1__CREATE_TABLE_USERS.sql
@@ -1,8 +1,7 @@
 CREATE TABLE users(
     id VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    apresentation VARCHAR(255) NOT NULL,
     address_cep VARCHAR(255) NOT NULL
 );
